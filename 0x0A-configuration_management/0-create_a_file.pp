7b6140375893a16edad292ec72a063395fdd8cf1
bayisa2022 <bayisa2022@gmail.com>
2023-04-04 00:37:45 -0700

    Task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
-file { '/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
-  content => 'I love Puppet',
+file {'/tmp/school':
+    mode       => '0744',
+    owner      => www-data,
+    group      => www-data,
+    content    => 'I love Puppet'
 }
