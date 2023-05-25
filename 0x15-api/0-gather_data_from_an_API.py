4d356d90e447e9b268bea5d481528b2bef593496
bayisa <bayisa2022@gmail.com>
2023-05-25 03:14:39 -0700

    Task-0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
 import requests
-import sys
+import json
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_employee_todo_progress(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    todos = json.loads(response.text)
+    completed_todos = [todo for todo in todos if todo["completed"]]
+    total_todos = len(todos)
+    completed_todos_count = len(completed_todos)
+    progress = (completed_todos_count / total_todos) * 100
+    return f"Employee {employee_id} has completed {progress:.2f}% of their TODO list."
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+print(get_employee_todo_progress(1))
